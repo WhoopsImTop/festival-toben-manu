f356a227605fc8a282a814c70817845728e8a8a4
Elias Englen <eliasenglen2@gmail.com>
2023-04-28 14:44:22 +0200

    add new

--- pages/cancel.vue
+++ pages/cancel.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="content-container">
         <h1>Du hast die Bezahlung abgebrochen</h1>
-        <p>Versuche es bei <nuxt-link to="/ticket-kaufen">Ticket-kaufen</nuxt-link>erneut.</p>
+        <p>Versuche es bei <nuxt-link to="/ticket-kaufen">Ticket-kaufen</nuxt-link> erneut.</p>
     </div>
 </template>
 
